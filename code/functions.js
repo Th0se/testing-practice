7dd1ecd000d7bef4a12a5906fb1afc1befafadbf
Th0se <thomasap1464@gmail.com>
2023-03-26 04:05:21 +0700

    Add and test the following functions: 1. Calculator (finished). 2. Caesar Chiper.

--- code/functions.js
+++ code/functions.js
@@ -1,4 +1,4 @@
-//"use strict";
+"use strict";
 
 const capitalise = (string) => {
     // Capitalise the first character of the string.
@@ -49,10 +49,76 @@ const Calculator = (number) => {
     };
 };
 
-const device = Calculator(8);
-console.log(device);
+const caesarChiper = (string, shiftNumber) => {
+    // A function to encrypt a string using Caesar Chiper
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabetCapital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+
+    for (let i = 0; i < string.length; i++) {
+        const currentCharacter = string[i];
+        if (alphabetCapital.includes(currentCharacter)) {
+            const index = alphabetCapital.indexOf(currentCharacter);
+            let shiftedIndex = (index + shiftNumber) % alphabetCapital.length;
+            if (shiftedIndex < 0) shiftedIndex += alphabetCapital.length;
+            result += alphabetCapital[shiftedIndex];
+        } else if (alphabet.includes(currentCharacter)) {
+            const index = alphabet.indexOf(currentCharacter);
+            let shiftedIndex = (index + shiftNumber) % alphabet.length;
+            if (shiftedIndex < 0) shiftedIndex += alphabet.length;
+            result += alphabet[shiftedIndex];
+        } else {
+            result += currentCharacter;
+        };
+    };
+
+    return result;
+};
+
+const analyzeArray = (array) => {
+    const calculateAverage = (array) => {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        };
+        let average = sum / (array.length - 1);
+        return average;
+    };
+
+    const calculateMin = (array) => {
+        let min = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            };
+        };
+        return min;
+    };
+
+    const calculateMax = (array) => {
+        let max = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            };
+        };
+        return max;
+    };
+
+    return {
+        average: Math.floor(calculateAverage(array)),
+        min: calculateMin(array),
+        max: calculateMax(array),
+        length: array.length,
+    };
+};
+
+console.log(analyzeArray([1, 8, 3, 4, 2, 6]))
+
 export {
     capitalise,
     reverseString,
     Calculator,
+    caesarChiper,
+    analyzeArray,
 }
--- test/functions.test.js
+++ test/functions.test.js
@@ -2,6 +2,8 @@ import {
     capitalise,
     reverseString,
     Calculator,
+    caesarChiper,
+    analyzeArray,
 } from '../code/functions.js';
 
 describe(`capitalise function`, () => {
@@ -28,5 +30,48 @@ describe(`Calculator factory`, () => {
         let device = Calculator(8);
         device.add(2);
         expect(device.value).toBe(10);
+    });
+    test(`Should be 6`, () => {
+        let device = Calculator(8);
+        device.substract(2);
+        expect(device.value).toBe(6);
+    });
+    test(`Should be 4`, () => {
+        let device = Calculator(8);
+        device.divide(2);
+        expect(device.value).toBe(4);
+    });
+    test(`Should be 16`, () => {
+        let device = Calculator(8);
+        device.multiply(2);
+        expect(device.value).toBe(16);
+    });
+});
+
+describe(`Caesar chiper`, () => {
+    test(`Should be 'cplkpi`, () => {
+        expect(caesarChiper(`anjing`, 2)).toBe(`cplkpi`);
+    });
+    test(`Should be 'Slslw'`, () => {
+        expect(caesarChiper(`Pipit`, 3)).toBe(`Slslw`);
+    });
+    test(`Should be 'CPLKPI'`, () => {
+        expect(caesarChiper(`ANJING`, 2)).toBe(`CPLKPI`);
+    });
+    test(`Should be 'slslW'`, () => {
+        expect(caesarChiper(`pipiT`, 3)).toBe(`slslW`);
+    });
+});
+
+describe(`Analyze`, () => {
+    test(`object test`, () => {
+        expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual(
+            {
+                average: 4,
+                min: 1,
+                max: 8,
+                length: 6
+            }
+        );
     });
 });
